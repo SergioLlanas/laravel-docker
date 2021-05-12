c77a8fefe79956924a6cbfeaee4f26f5d4c835cb
laamaai <maiderp1a@gmail.com>
2021-05-12 11:03:37 +0200

    Three new test in the file openWalletControllerTest done

--- src/app/Http/Controllers/OpenWalletController.php
+++ src/app/Http/Controllers/OpenWalletController.php
@@ -5,39 +5,43 @@ namespace App\Http\Controllers;
 
 
 use App\DataSource\database\WalletDataSource;
+use App\Services\GetWalletService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Mockery\Exception;
 
 class OpenWalletController extends BaseController {
     /**
      * @var WalletDataSource
      */
-    private $walletDAO;
+    private $getWalletService;
 
 
     /**
      * OpenWalletController constructor.
      */
-    public function __construct(WalletDataSource $walletDAO){
-
-        $this->walletDAO = $walletDAO;
-
+    public function __construct(GetWalletService $getWalletService){
+        $this->getWalletService = $getWalletService;
     }
 
     public function __invoke(Request $request): JsonResponse{
-
-       $userId = $request->input("user_id");
-       $this->walletDAO->createNewWalletWithUserId($userId);
-       $walletWithMaxId = $this->walletDAO->getWalletWithMaxId();
-
-
+        try{
+            $userId = $request->input("user_id");
+            if(is_null($userId)){
+                return response()->json([
+                    'error' => 'User not found'
+                ], Response::HTTP_BAD_REQUEST);
+            }
+            $wallet = $this->getWalletService->open($userId);
+        }catch(Exception $exception){
+            return response()->json([
+                'error' => $exception->getMessage()
+            ], Response::HTTP_BAD_REQUEST);
+        }
         return response()->json([
-            'wallet_id' => $walletWithMaxId->wallet_id
+            'wallet_id' => $wallet
         ], Response::HTTP_OK);
-
-
-
     }
 }
--- src/tests/Integration/Controller/OpenWalletControllerTest.php
+++ src/tests/Integration/Controller/OpenWalletControllerTest.php
@@ -2,21 +2,42 @@
 
 namespace Tests\Integration\Controller;
 
+use App\Models\Wallet;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
 use Tests\TestCase;
 
 class OpenWalletControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test  */
+    public function openWalletForValidUserId(){
+        Wallet::factory(Wallet::class)->create();
+
+        $response = $this->postJson('/api/wallet/open', ['user_id' => '1']);
+
+        $response->assertStatus(Response::HTTP_OK)->assertExactJson(['wallet_id' => '2']);
     }
+
+    /** @test */
+    public function walletNotOpenForNullUser(){
+        Wallet::factory(Wallet::class)->create();
+
+        $response = $this->postJson('/api/wallet/open');
+
+        $response->assertStatus(Response::HTTP_BAD_REQUEST)->assertExactJson(['error'=>'User not found']);
+    }
+
+    /** @test */
+    public function walletNotOpenForEmptyUser(){
+        Wallet::factory(Wallet::class)->create();
+
+        $response = $this->postJson('/api/wallet/open', ['user_id' => '']);
+
+        $response->assertStatus(Response::HTTP_BAD_REQUEST)->assertExactJson(['error'=>'User not found']);
+    }
+
+
+
 }
